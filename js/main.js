a4867f5b234c234c0ad330d331191ba6135838c6
Diomar <diomarpr@gmail.com>
2022-06-10 13:10:44 -0300

    update

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 let btn = document.querySelector("#icon-logo");
 let sidebar = document.querySelector(".sidebar");
 
+
+
+// Eventos
 btn.onclick = function () {
   sidebar.classList.toggle("active");
 }
-
-
 
 
